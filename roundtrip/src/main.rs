82edcffd8543e9d78eba4dac5b216f329d78a3c8
Aurora <tomek-kubel2@wp.pl>
2022-09-07 15:36:50 +0200

    split roundtrip's cli into encode and decode

--- roundtrip/src/main.rs
+++ roundtrip/src/main.rs
@@ -12,61 +12,74 @@ use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
 use pulsejet_rs::Std;
 
 #[derive(Parser)]
-struct Opts {
-    #[clap(short, long)]
-    bit_rate: f64,
-    input: PathBuf,
-    output: PathBuf,
+enum Opts {
+    Encode {
+        #[clap(short, long)]
+        bit_rate: f64,
+        input: PathBuf,
+        output: PathBuf,
+    },
+    Decode {
+        input: PathBuf,
+        output: PathBuf,
+    },
 }
 
 fn main() {
     eprintln!("start");
-    let opts = Opts::parse();
-
-    let reader = WavReader::open(opts.input).unwrap();
-    let spec = reader.spec();
-    assert_eq!(spec.channels, 2);
-    assert_eq!(spec.sample_rate, 44100);
-    let max_value = 1i32
-        .checked_shl(spec.bits_per_sample as u32)
-        .and_then(|x| x.checked_sub(1))
-        .unwrap_or(i32::MAX);
-    let samples: Vec<f32> = match spec.sample_format {
-        SampleFormat::Float => reader
-            .into_samples::<f32>()
-            .array_chunks::<2>()
-            .map(|[a, b]| Ok::<_, hound::Error>((a? + b?) / 2.0))
-            .collect::<Result<_, _>>(),
-        SampleFormat::Int => reader
-            .into_samples::<i32>()
-            .map(|x| x.map(|x| x as f32 / max_value as f32))
-            .array_chunks::<2>()
-            .map(|[a, b]| Ok((a? + b?) / 2.0))
-            .collect::<Result<_, _>>(),
+    match Opts::parse() {
+        Opts::Encode {
+            bit_rate,
+            input,
+            output,
+        } => {
+            let reader = WavReader::open(input).unwrap();
+            let spec = reader.spec();
+            assert_eq!(spec.channels, 2);
+            assert_eq!(spec.sample_rate, 44100);
+            let max_value = 1i32
+                .checked_shl(spec.bits_per_sample as u32)
+                .and_then(|x| x.checked_sub(1))
+                .unwrap_or(i32::MAX);
+            let samples: Vec<f32> = match spec.sample_format {
+                SampleFormat::Float => reader
+                    .into_samples::<f32>()
+                    .array_chunks::<2>()
+                    .map(|[a, b]| Ok::<_, hound::Error>((a? + b?) / 2.0))
+                    .collect::<Result<_, _>>(),
+                SampleFormat::Int => reader
+                    .into_samples::<i32>()
+                    .map(|x| x.map(|x| x as f32 / max_value as f32))
+                    .array_chunks::<2>()
+                    .map(|[a, b]| Ok((a? + b?) / 2.0))
+                    .collect::<Result<_, _>>(),
+            }
+            .unwrap();
+            let result =
+                pulsejet_rs::encode::<Std>(&samples, 44100.0, bit_rate);
+            eprintln!("Total bits estimate: {}", result.total_bits_estimate);
+            let encoded = result.stream;
+            fs::write(output, &encoded).unwrap();
+        }
+        Opts::Decode { input, output } => {
+            let encoded = fs::read(input).unwrap();
+            let decoded = pulsejet_rs::decode::<Std>(&encoded);
+            let output = BufWriter::new(File::create(output).unwrap());
+            let mut writer = WavWriter::new(
+                output,
+                WavSpec {
+                    channels: 1,
+                    sample_rate: 44100,
+                    bits_per_sample: 32,
+                    sample_format: SampleFormat::Float,
+                },
+            )
+            .unwrap();
+            for sample in decoded {
+                writer.write_sample(sample).unwrap();
+            }
+            writer.finalize().unwrap();
+            eprintln!("finished");
+        }
     }
-    .unwrap();
-    let samples_raw = bytemuck::cast_slice(&samples);
-    fs::write("orig_rust.raw", samples_raw).unwrap();
-    let encoded =
-        pulsejet_rs::encode::<Std>(&samples, 44100.0, opts.bit_rate).stream;
-
-    fs::write("encoded_rust.pj", &encoded).unwrap();
-
-    let decoded = pulsejet_rs::decode::<Std>(&encoded);
-    let output = BufWriter::new(File::create(opts.output).unwrap());
-    let mut writer = WavWriter::new(
-        output,
-        WavSpec {
-            channels: 1,
-            sample_rate: 44100,
-            bits_per_sample: 32,
-            sample_format: SampleFormat::Float,
-        },
-    )
-    .unwrap();
-    for sample in decoded {
-        writer.write_sample(sample).unwrap();
-    }
-    writer.finalize().unwrap();
-    eprintln!("finished");
 }
